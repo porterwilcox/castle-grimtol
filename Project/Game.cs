8b675f554a7c63898359fdb94882c440e89469af
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-26 16:58:16 -0600

    two msbuilld file complications. got generic igame interface implemented

--- Project/Game.cs
+++ Project/Game.cs
@@ -2,7 +2,64 @@ using System.Collections.Generic;
 
 namespace CastleGrimtol.Project
 {
-  public class Game : IGame
-  {
-  }
+    public class Game : IGame
+    {
+        public Room CurrentRoom { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public Player CurrentPlayer { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void GetUserInput()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Go(string direction)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Help()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Inventory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Look()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Quit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Setup()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StartGame()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void TakeItem(string itemName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UseItem(string itemName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
